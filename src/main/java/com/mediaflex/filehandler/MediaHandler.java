b4bf12ceeaab6a88faf2e285afb1f2ec0833d2c7
mikoow <mikoow3@naver.com>
2020-10-02 21:25:26 +0900

    테스트 한번 해보자

---
+++ src/main/java/com/mediaflex/filehandler/MediaHandler.java
@@ -0,0 +1,16 @@
+package com.mediaflex.filehandler;
+
+public class MediaHandler implements Runnable {
+
+	public MediaHandler() {
+
+	}
+
+	@Override
+	public void run() {
+		TestHandler th = new TestHandler();
+
+	}
+
+
+}
