6869dcf90defbd36274839525c5249073e45d3d8
mikoow <mikoow3@naver.com>
2020-10-02 20:07:51 +0900

    test_branch first commit

---
+++ src/main/java/com/mediaflex/filehandler/TestHandler.java
@@ -0,0 +1,17 @@
+package com.mediaflex.filehandler;
+
+/**
+ * @author      : namwoo (namwoo@KIMNAS)
+ * @file        : TestHandler
+ * @created     : 금요일 10월 02, 2020 20:06:52 KST
+ */
+
+public class TestHandler
+{
+    public TestHandler()
+    {
+        
+    }
+}
+
+
